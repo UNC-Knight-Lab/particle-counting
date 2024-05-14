e0b29ff017adab074e99ca7e217cc4b02de6e3b3
ballenp <122381077+ballenp@users.noreply.github.com>
2024-05-14 15:18:04 -0400

    Update Histogram.m
    More comments, more streamlined to insert user data by only changing the variable name in line 3

--- Histogram.m
+++ Histogram.m
@@ -1,34 +1,31 @@
-%% 
-bin_centers = [1,3,5,7,9,11,13,15,17,19,21,23,25,27,29]; %generate 1 column vector for bin centers
-edges = (0:2:30);
-% tBAXTEN2_10 = table2array(tBAXTEN210measurementfile);
-% tBAXTEN2_12 = table2array(tBAXTEN212measurementfile);
-% tBAXTEN2_18 = table2array(tBAXTEN218measurementfile);
-% tBAXTEN2_19 = table2array(tBAXTEN219measurementfile);
-% tBAXTEN2_9 = table2array(tBAXTEN29measurementfile);
+%% Create histogram of 2nm bins from 0 to 50 nm
 
-%% 
+X = Combined_measurement_file %insert name of combined measurement file made from Script_Polymer_Analysis
+bin_centers = [1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,47,49]; %generate 2 nm bin columns 
+edges = [0:2:50]; %make edges between 0 and 50 spaced by intervals of 2
+hist = histcounts(X, edges); %function to generate histogram to check particle diatmeter, 
 
-bin_centers_column = bin_centers.'
-% CHAXTEN2_10_Frequency = histcounts(CHA_XTEN2_10_measurementfile,edges);
-% CHAXTEN2_10_column = CHAXTEN2_10_Frequency.';
-% CHA_XTEN2_12_Frequency = histcounts(CHA_XTEN2_12_measurementfile,edges);
-% CHA_XTEN2_12_column = CHA_XTEN2_12_Frequency.';
-% CHA_XTEN2_7_Frequency = histcounts(CHA_XTEN2_7_measurementfile,edges);
-% CHA_XTEN2_7_column = CHA_XTEN2_7_Frequency.';
-EA_XTEN2_17_Frequency = histcounts(EA_XTEN2_17_measurementfile,edges);
-EA_XTEN2_17_column = EA_XTEN2_17_Frequency.';
-%%                                                                                                                                                                     
-Combined_Hist_Columns = [EA_XTEN2_17_column];
+%% Translate row files to columns 
+
+bin_centers_column = bin_centers.' %translates row file to column
+hist_column = hist.' %translates row file to column
+
+%% Create normalized histogram text file of measurement file plotting bin centers 
+
 Total_Freq = sum(Combined_Hist_Columns,2);
-Table = table (bin_centers_column, Total_Freq);
-writetable(Table,'EA-XTEN2.txt','Delimiter','space')
-%% 
-
-
-%figure()
-%edges =[20:2:50]
-%histogram(Freq_1321_column,edges)
-
+Freq_max = max(Total_Freq);
+Normal_Freq = Total_Freq/Freq_max;
+Table = table (bin_centers_column, Normal_Freq);
+writetable(Table,'nBA5_XTEN2_sonication_nospaghet_2nmbins.txt','Delimiter','space');
+%% Histogram figure 
+figure();
+edges =[0:2:50];
+histogram(X,edges);
+title('Put Your Title Here');
+set(gca,'box','off');
+xlabel('Diameter (nm)');
+ylabel('Count');
 
 %% Finding average particle size and standard deviation
+Particle_mean = mean(X)
+Particle_std = std(X)
